09430b27da70ec913e1c47e29d91898bff60891c
lfkdsk <lfk_dsk@hotmail.com>
2017-07-05 03:08:12 +0800

    update simple buffer

--- src/buffer/SimpleBufferManagerImpl.kt
+++ src/buffer/SimpleBufferManagerImpl.kt
@@ -4,35 +4,53 @@ import core.JustDB
 import storage.Block
 
 /**
+ * SimpleBuffer-Manager
+ * control block <=> buffer
+ * @param justDB DataBase
+ * @param bufferNumber buffer-number
  * Created by liufengkai on 2017/4/30.
  */
 class SimpleBufferManagerImpl(justDB: JustDB, bufferNumber: Int) : BufferManager {
-
+	/**
+	 * initial buffer-pool
+	 */
 	private val bufferPool: Array<Buffer> = Array(bufferNumber, { Buffer(justDB) })
+
 	private var numAvailable: Int = bufferNumber
 
+	/**
+	 * flush all in bufferPool
+	 * @param transaction transaction Num
+	 */
 	@Synchronized
 	override fun flushAll(transaction: Int) {
-		bufferPool.filter { it.isModifiedBy(transaction) }
+		bufferPool.filter { buffer -> buffer.isModifiedBy(transaction) }
 				.forEach(Buffer::flush)
 	}
 
-	override fun available(): Int {
-		return numAvailable
-	}
+	override fun available(): Int = numAvailable
 
+	/**
+	 * pin block => buffer manager
+	 * @param block block <=> buffer
+	 */
 	@Synchronized override
 	fun pin(block: Block): Buffer? {
+		// exist buffer
 		var buffer = getExistBuffer(block)
 		if (buffer == null) {
+			// unused buffer
 			buffer = chooseUnPinnedBuffer()
+			// double-check
 			if (buffer == null)
 				return null
 			buffer.assignToBlock(block)
 		}
+
 		if (!buffer.isPinned())
-			numAvailable--
+			numAvailable.inv()
 		buffer.pin()
+
 		return buffer
 	}
 
@@ -52,12 +70,22 @@ class SimpleBufferManagerImpl(justDB: JustDB, bufferNumber: Int) : BufferManager
 		return buff
 	}
 
-
+	/**
+	 * Exist buffer
+	 * @param block bind block
+	 */
 	private fun getExistBuffer(block: Block): Buffer? {
-		return bufferPool.firstOrNull { it.block() != null && it.block() == block }
+		return bufferPool.firstOrNull { buffer ->
+			buffer.block() != null && buffer.block() == block
+		}
 	}
 
+	/**
+	 * choose unpin buffer
+	 */
 	private fun chooseUnPinnedBuffer(): Buffer? {
-		return bufferPool.firstOrNull { !it.isPinned() }
+		return bufferPool.firstOrNull { buffer ->
+			!buffer.isPinned()
+		}
 	}
 }
